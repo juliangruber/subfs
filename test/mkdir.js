8691bd020fc44d3e75d087a83c7609eaf91ea0dd
Julian Gruber <julian@juliangruber.com>
2013-10-08 14:29:44 +0100

    added mkdir test

---
+++ test/mkdir.js
@@ -0,0 +1,28 @@
+var test = require('tap').test;
+var fs = require('fs');
+var subfs = require('..');
+var os = require('os');
+var join = require('path').join;
+
+test('mkdir', function(t) {
+  t.plan(2);
+  var dir = join(os.tmpdir(), Math.random().toString(16).slice(2));
+
+  fs.mkdirSync(dir);
+
+  subfs(fs, dir).mkdir('dir', function(err) {
+    t.error(err);
+    t.ok(fs.existsSync(join(dir, 'dir')));
+  });
+});
+
+test('mkdirSync', function(t) {
+  t.plan(1);
+  var dir = join(os.tmpdir(), Math.random().toString(16).slice(2));
+
+  fs.mkdirSync(dir);
+
+  subfs(fs, dir).mkdirSync('dir');
+  t.ok(fs.existsSync(join(dir, 'dir')));
+});
+
