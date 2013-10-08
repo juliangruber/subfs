854d86fa88b493f8bd5d6a0904c7d3603ca5eb03
Julian Gruber <julian@juliangruber.com>
2013-10-08 14:36:20 +0100

    added readdir test

---
+++ test/readdir.js
@@ -0,0 +1,27 @@
+var test = require('tap').test;
+var fs = require('fs');
+var subfs = require('..');
+var os = require('os');
+var join = require('path').join;
+
+test('readdir', function(t) {
+  t.plan(2);
+  var dir = join(os.tmpdir(), Math.random().toString(16).slice(2));
+  fs.mkdirSync(dir);
+  fs.writeFileSync(join(dir, 'file.txt'), 'foobar');
+
+  subfs(fs, dir).readdir('/', function(err, files) {
+    t.error(err);
+    t.deepEqual(files, ['file.txt']);
+  });
+});
+
+test('readdirSync', function(t) {
+  t.plan(1);
+  var dir = join(os.tmpdir(), Math.random().toString(16).slice(2));
+  fs.mkdirSync(dir);
+  fs.writeFileSync(join(dir, 'file.txt'), 'foobar');
+
+  t.deepEqual(subfs(fs, dir).readdirSync('/'), ['file.txt']);
+});
+
